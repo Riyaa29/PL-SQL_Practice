0a6babb87f9d607d458b44f47ae37b26e45068f0
Riya Nagpal <30284299+Riyaa29@users.noreply.github.com>
2023-01-19 10:51:11 -0500

    Update Project8.sql

--- Projects/Project8/Project8.sql
+++ Projects/Project8/Project8.sql
@@ -2,12 +2,7 @@
   @/tmp/oracle/Project8/Project8.sql
 */
 
-/*
-Name: Riya Nagpal
-ID: 2220097
-Date: 2022/11/22
-Description - Databases 2 - Project 8
-*/
+
 
 SPOOL /tmp/oracle/Project8/Project8_SPOOL.txt
 
